aed77e0d15d08d4df6d3a5a789de161dc846ea88
Phushyamithra <gauriphushyamithra@gmail.com>
2024-03-21 11:02:56 +0530

    Addition of the change in the text of add to cart button is made here

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,17 @@
 import cartIcon from '.././assets/icons/cart.svg'
+import {useState} from "react"
 
+// click on add to cart action 
 
 const Item = ({data})=>{
+
+    const [cartMessage, setCartMessage] = useState("Product not added to cart yet !! ");
+    const clickOnCart = () => {
+        setCartMessage("Product added to cart.");
+        console.log("Clicked");
+    }
+
+
     return (
     <div className={"item-card"}>
             <img src={data.thumbnail} cclassName={"img-fluid"} alt="Page shirt item" />
@@ -14,7 +24,8 @@ const Item = ({data})=>{
                     <h3>{data.productName}</h3>
                 </div>
             </div>
-            <button className={"cart-add"}>
+            <small className={"cart-message"}>{cartMessage}</small>
+            <button className={"cart-add"} onClick={clickOnCart}>
                 <span>Add to Cart</span>
                 <img src={cartIcon} alt="Cart logo" />
             </button>

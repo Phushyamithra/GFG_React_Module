eeb08058cce36b3769a498fe86baa87e90061fd0
Phushyamithra <gauriphushyamithra@gmail.com>
2024-03-23 00:07:31 +0530

    Final day commit of 23-4-2024 for Form handling and two way binding

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,9 @@
 import { useState } from "react"
 import Item from "./Item"
-
+import Form from "./Form"
 
 const ProductList = () => {
-    const [title, setTitle] = useState("")
-    const [price, setPrice] = useState(0)
-    const [discountedPrice, setDiscountedPrice] = useState(0)
-    const [thumbnail, setThumbnail] = useState("")
+
 
     const [item, setItem] = useState({
         id: 0,
@@ -15,110 +12,26 @@ const ProductList = () => {
         discountedPrice: 340,
         thumbnail: "shirt.png"
     })
-
-    const handleTitle = (event) => {
-        // console.log(event)
-        // console.log(event.target.value)
-        setTitle(event.target.value)
-        setItem({
-            ...item,
-            title: event.target.value
-        })
+    const handleChange = (event) => {
+      setItem({
+        ...item,
+        [event.target.name] : event.target.value
+      })
     }
-
-    const handlePrice = event => {
-        setPrice(event.target.value)
-        setItem({
-            ...item,
-            price: event.target.value
-        })
-    }
-
-    const handleDiscountedPrice = event => {
-        setDiscountedPrice(event.target.value)
-        setItem({
-            ...item,
-            discountedPrice: event.target.value
-        })
-    }
-
-    const handleThumbnail = event => {
-        setThumbnail(event.target.value)
-        setItem({
-            ...item,
-            thumbnail: event.target.value
-        })
-    }
-
     const submitForm = event => {
         event.preventDefault();
-        console.log({
-            title: title,
-            price,
-            discountedPrice,
-            thumbnail
-        })
-        if(discountedPrice > price) {
+        
+        if(item.discountedPrice > item.price) {
             alert("Discounted Price cannot be greater than price")
             return;
         }
-        setItem({
-            title,
-            price,
-            discountedPrice,
-            thumbnail
-        })
+        console.log("Data from Form came back "+item);
     }
 
     return (
         <div className={"product-wrapper"}>
             <div className={"form"}>
-                <form onSubmit={submitForm}>
-                    <h2>Item Card Details</h2>
-                    <div className={"input-field"}>
-                        <label htmlFor="title">Title</label>
-                        <input 
-                            type="text" 
-                            placeholder="Enter Title" 
-                            value={title} 
-                            onChange={handleTitle}
-                            required
-                        />
-                    </div>
-                    <div className={"input-field"}>
-                        <label htmlFor="price">Price</label>
-                        <input 
-                            type="number" 
-                            placeholder="Enter Price" 
-                            value={price} 
-                            onChange={handlePrice}
-                            required
-                        />
-                    </div>
-                    <div className={"input-field"}>
-                        <label htmlFor="discountPrice">Discount Price</label>
-                        <input 
-                            type="number" 
-                            placeholder="Enter Discounted Price" 
-                            value={discountedPrice} 
-                            onChange={handleDiscountedPrice}
-                            required
-                        />
-                    </div>
-                    <div className={"input-field"}>
-                        <label htmlFor="thumbnail">Thumbnail</label>
-                        <input 
-                            type="text" 
-                            placeholder="Enter Thumbnail name" 
-                            value={thumbnail} 
-                            onChange={handleThumbnail}
-                            required
-                        />
-                    </div>
-                    <div className={"submit-wrap"}>
-                        <button>Update</button>
-                    </div>
-                </form>
+                <Form item = {item} onInputChange ={handleChange} onFormSubmission = {submitForm}></Form>
             </div>
             <div>
                 <div>

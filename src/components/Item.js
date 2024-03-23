18df01ee7ed492042c0bb28e085db4fe34d84200
Phushyamithra <gauriphushyamithra@gmail.com>
2024-03-23 15:11:54 +0530

    Start commit with map function usage 23-3-24

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,50 +1,27 @@
 import cartIcon from '.././assets/icons/cart.svg'
-import {useState} from "react"
 
 // click on add to cart action 
 
-const Item = ({data})=>{
+const Item = ({ data }) => {
 
-    const [counter,setCounter] = useState(0);
-
-    const descreaseCounterByOne = () => {
-      let count= counter-1;
-      if(count<0){
-        return;
-      }
-      setCounter(count);
-    }
-
-    const increaseCounterByOne = () => {
-      let count = counter+1;
-      setCounter(count);
-    }
-    return (
-
+  return (
     <div className={"item-card"}>
-            <img src={data.thumbnail} className={"img-fluid"} alt="Page shirt item" />
-            <div className={"item-card__information"}>
-                <div className="pricing">
-                    <span>{data.discountedPrice}</span>
-                    <small><strike>{data.price}</strike></small>
-                </div>
-                <div className={"title"}>
-                    <h3>{data.title}</h3>
-                </div>
-            </div>
-            {/* <button className={"cart-add"} onClick={clickOnCart}>
-                <span>Add to Cart</span>
-                <img src={cartIcon} alt="Cart logo" />
-            </button>
-             */}
-            <div className={"cart-addon"}>
-                <button onClick={descreaseCounterByOne}><span>-</span></button>
-                <span className={"counter"}>{counter}</span>
-                <button onClick={increaseCounterByOne}><span>+</span></button>
-            </div>
-
+      <img src={data.thumbnail} className={"img-fluid"} alt="Page shirt item" />
+      <div className={"item-card__information"}>
+        <div className="pricing">
+          <span>{data.discountedPrice}</span>
+          <small><strike>{data.price}</strike></small>
+        </div>
+        <div className={"title"}>
+          <h3>{data.title}</h3>
+        </div>
+      </div>
+      <button className={"cart-add"} >
+        <span>Add to Cart</span>
+        <img src={cartIcon} alt="Cart logo" />
+      </button>
     </div>
-    );
+  );
 }
 
 export default Item;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,42 +1,48 @@
 import { useState } from "react"
 import Item from "./Item"
-import Form from "./Form"
 
 const ProductList = () => {
 
 
-    const [item, setItem] = useState({
+    const [items, setItems] = useState([{
         id: 0,
         title: "Title of this Item 1",
         price: 450,
         discountedPrice: 340,
         thumbnail: "shirt.png"
-    })
-    const handleChange = (event) => {
-      setItem({
-        ...item,
-        [event.target.name] : event.target.value
-      })
-    }
-    const submitForm = event => {
-        event.preventDefault();
-        
-        if(item.discountedPrice > item.price) {
-            alert("Discounted Price cannot be greater than price")
-            return;
-        }
-        console.log("Data from Form came back "+item);
-    }
+    },
+    {
+        id: 1,
+        title: "White Shirt",
+        price: 245,
+        discountedPrice: 230,
+        thumbnail: "shirt.png",
+    },
+    {
+        id: 2,
+        title: "Black Hoodie",
+        price: 245,
+        discountedPrice: 230,
+        thumbnail: "shirt.png",
+    },
+    {
+        id: 3,
+        title: "Black Shirt",
+        price: 245,
+        discountedPrice: 230,
+        thumbnail: "shirt.png",
+    }]
+    );
+    
 
     return (
-        <div className={"product-wrapper"}>
-            <div className={"form"}>
-                <Form item = {item} onInputChange ={handleChange} onFormSubmission = {submitForm}></Form>
-            </div>
-            <div>
-                <div>
-                    <Item data={item} />
-                </div>
+        <div className={"product-list"}>
+            <div className={"product-list--wrapper"}>
+               {
+                    items.map((item) => {
+                      return(<Item data ={item}/>);    
+                    })
+                }
             </div>
         </div>
     )

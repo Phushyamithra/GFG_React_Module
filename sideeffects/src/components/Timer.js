f8c555cf4fabc9138773f3df8ef52b0c205d375e
Phushyamithra <gauriphushyamithra@gmail.com>
2024-03-29 17:33:16 +0530

    commit after side effects completion

--- sideeffects/src/components/Timer.js
+++ sideeffects/src/components/Timer.js
@@ -3,11 +3,14 @@ import { useEffect, useState } from "react";
 const Timer = () => {
     const[count,setCount] = useState(0);
     useEffect(() => {
-        setInterval(() => {
-            setCount(count+1);
+        const timeInterval = setInterval(() => {
+            setCount(prevoiusVal => prevoiusVal+1);
         }, 1000);
-      
-    },[])
+        return () => {
+            console.log("Timer unmounted");
+            clearInterval(timeInterval)
+        }
+    },[count])
     return(
     <div>
         <h1>{count} Seconds</h1>
